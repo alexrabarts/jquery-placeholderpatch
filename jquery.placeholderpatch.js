dc701e632339c04307cb2a8e8bd80888d229d407
Alex Rabarts <alexrabarts@gmail.com>
2013-11-25 12:16:37 +0000

    Test for placeholder support before applying patch

--- jquery.placeholderpatch.js
+++ jquery.placeholderpatch.js
@@ -10,48 +10,56 @@
   $.extend($.fn, {
     placeholder: function (options) {
       var defaults = {placeholderClass: 'placeholder'};
+      var test = document.createElement('input');
 
       options = $.extend(defaults, options);
 
-      return this.each(function () {
-        var input = $(this).addClass(options.placeholderClass);
-        var form = input.parents('form:first');
-        var placeholder = input.attr('placeholder');
+      $.support.placeholder = false;
 
-        if (placeholder) {
-          if (input.val() === '') {
-            input.val(placeholder);
+      if('placeholder' in test) {
+        $.support.placeholder = true;
+        return function() {}
+      } else {
+        return this.each(function () {
+          var input = $(this).addClass(options.placeholderClass);
+          var form = input.parents('form:first');
+          var placeholder = input.attr('placeholder');
+
+          if (placeholder) {
+            if (input.val() === '') {
+              input.val(placeholder);
+            }
+
+            input.focus(function () {
+              clearInput();
+            }).blur(function () {
+              unclearInput();
+            });
+
+            form.submit(function() {
+              if (input.hasClass(options.placeholderClass)) {
+                input.val('');
+              }
+            });
+
+            input.blur();
           }
 
-          input.focus(function () {
-            clearInput();
-          }).blur(function () {
-            unclearInput();
-          });
-
-          form.submit(function() {
-            if (input.hasClass(options.placeholderClass)) {
+          function clearInput() {
+            if (input.val() === placeholder) {
               input.val('');
             }
-          });
 
-          input.blur();
-        }
-
-        function clearInput() {
-          if (input.val() === placeholder) {
-            input.val('');
+            input.removeClass(options.placeholderClass);
           }
 
-          input.removeClass(options.placeholderClass);
-        }
-
-        function unclearInput() {
-          if (input.val() === '') {
-            input.addClass(options.placeholderClass).val(placeholder);
+          function unclearInput() {
+            if (input.val() === '') {
+              input.addClass(options.placeholderClass).val(placeholder);
+            }
           }
-        }
-      });
+        });
+      }
     }
   });
 })(jQuery);
